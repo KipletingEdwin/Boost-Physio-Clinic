2bfe481bc8b11fe9bbaae723b560e7c439433573
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-19 16:04:02 +0000

    Add int id,String name, String address, String phone and two Lists: expertise and timetable

--- src/Physiotherapist.java
+++ src/Physiotherapist.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class Physiotherapist {
+
+    public  int id;
+    public String name;
+    public String address;
+    public String phone;
+
+    public  Physiotherapist(int id, String name, String address, String phone){
+
+        List<String> expertise;
+        List<String> timetable;
+
+    }
 }
