ef5a72f9ecdc6af385d85f066930f214bf58da4f
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-23 09:51:51 +0000

    Add an instance of a GUI window

--- src/GUI.java
+++ src/GUI.java
@@ -3,12 +3,19 @@ import javax.swing.*;
 public class GUI extends JFrame {
 
 
+        public static void main(String[] args) {
+
+
+
+    }
+
+
     public  GUI() {
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Boost Physio Clinic Booking System");
-
-
+        setSize(600, 400);
+        setVisible(true);
     }
 
 }
