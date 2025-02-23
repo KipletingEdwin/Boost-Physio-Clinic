cd3e6ccae8d7f6d77daf21ada567b0ac44360d0c
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-23 10:15:42 +0000

    Add getters in the physiotherapist class

--- src/Physiotherapist.java
+++ src/Physiotherapist.java
@@ -6,17 +6,32 @@ public class Physiotherapist {
     public String name;
     public String address;
     public String phone;
+    private  List<String> expertise;
 
     public  Physiotherapist(int id, String name, String address, String phone){
-//        List<String> expertise;
-//        List<String> timetable;
-
         this.id = id;
         this.name = name;
         this.address = address;
         this.phone = phone;
-
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public List<String> getExpertise() {
+        return expertise;
+    }
 }
