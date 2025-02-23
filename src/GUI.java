06311c0132f840c8b9882db002346ebbc1df1b3f
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-02-23 09:55:59 +0000

    Create an instance of a panel

--- src/GUI.java
+++ src/GUI.java
@@ -1,21 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI extends JFrame {
 
-
-        public static void main(String[] args) {
-
-
-
-    }
-
-
-    public  GUI() {
-
+    public GUI() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Boost Physio Clinic Booking System");
         setSize(600, 400);
         setVisible(true);
+
+        JPanel panel = new JPanel();
+
+
+
+
+
     }
 
 }
